3fdc3c49a7f6c80b3b0371ac31176e33663cc955
Kasope Johnson <kasopej@gmail.com>
2021-11-22 12:22:57 +0100

    Implemented catch for failed API request

--- app/Services/API_Service.js
+++ app/Services/API_Service.js
@@ -1,7 +1,9 @@
 export default class API_ServiceClass {
     async fetchData(url) {
-        const response = await fetch(url)
-        const resolvedData = await response.json();
+        let resolvedData;
+        const response = await fetch(url).catch((() => alert('Please check your connection and retry')))
+        if (response.ok) { resolvedData = await response.json() }
+        else { console.log('Response is not OK'); }
         return resolvedData;
     }
     post() { }
