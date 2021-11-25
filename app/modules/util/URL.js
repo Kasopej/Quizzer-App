2741ccf0956c6e65230d8fde701bfc3d0344a33f
Kasope Johnson <kasopej@gmail.com>
2021-11-25 02:22:27 +0100

    Added qlobal question quantity URL for query

--- app/modules/util/URL.js
+++ app/modules/util/URL.js
@@ -1,5 +1,6 @@
 export const BaseURL = 'https://opentdb.com/';
 export const QuestionsURL = 'https://opentdb.com/api.php?'
 export const CategoriesURL = 'https://opentdb.com/api_category.php';
+export const globalQtyOfQuestionsURL = 'https://opentdb.com/api_count_global.php'
 export const qtyOfQuestionsInCategoryURL = 'https://opentdb.com/api_count.php?category='
 export const quizPageRelativePath = '/quiz.html?';
