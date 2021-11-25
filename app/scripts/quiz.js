f805840b3865bb28d2f70bcf2e3af884cd62ec30
Kasope Johnson <kasopej@gmail.com>
2021-11-25 14:03:23 +0100

    Started refactoring of quiz.js script, and corrected class names in quiz html

--- app/scripts/quiz.js
+++ app/scripts/quiz.js
@@ -23,6 +23,7 @@ if (!sessionStorageQuestions && !sessionStorageConfigData) {
     quizzerData.updateConfigData(...Object.entries(params));
     sessionStoragePersistenceService.saveData('quizzer config data', params);
     const questions = await API_Service.fetchData(`${QuestionsURL}amount=${params.numberOfQuestions}&category=${params.selectedCategoryId}`).then(data => data.results);
+    console.log(questions);
     quizzerData.updateData(['questions data', quizzerMiddleWare.convertIncomingQuestionDataArray(questions)]);
     sessionStoragePersistenceService.saveData('questions data', quizzerData.getData('questions data'));
 }
@@ -33,20 +34,21 @@ else {
 
 let questionIndex = 0;
 let questionsData = quizzerData.getData('questions data');
+console.log(questionsData);
 if (quizzerDataOperation.checkIfQuizIsTimed()) {
     quizzerDataOperation.calcTotalTime();
-    quizzerDataOperation.updateAndRenderTimeLeft(UI_Interface.getElements('.timer span')[0]);
+    quizzerDataOperation.updateAndRenderTimeLeft(UI_Interface.getElements('#timer')[0]);
 }
 
 function renderQuizOnUI() {
-    UI_Interface.replaceHTML([UI_Interface.getElements('.card-text')[0]], [decodeURI(questionsData[questionIndex].question)])
-    UI_Interface.attachText([UI_Interface.getElements('.card-title')[0]], [`Q${questionIndex + 1} of ${questionsData.length}`])
-    let elementsCreated = UI_Interface.createElements(...'p'.repeat(questionsData[questionIndex].answers.length).split(''));
+    UI_Interface.replaceHTML([UI_Interface.getElements('#question')[0]], [decodeURI(questionsData[questionIndex].question)])
+    UI_Interface.attachText(Array.from(UI_Interface.getElements('.quizNumberTracker h2')), ['Question', `${questionIndex + 1}`, `${questionsData.length}`]);
+    let elementsCreated = UI_Interface.createElements(...'li '.repeat(questionsData[questionIndex].answers.length).split(' ').slice(0, questionsData[questionIndex].answers.length));
     elementsCreated.forEach((element, index) => {
         UI_Interface.replaceHTML([element], [`<label><input type="radio" name="option" value="${index + 1}" id="${index + 1}">${decodeURI(questionsData[questionIndex].answers[index].answer)}</label>`]);
     });
-    UI_Interface.replaceChildren(UI_Interface.getElements('.answer-options')[0], elementsCreated);
-    const optionElementsArray = Array.from(UI_Interface.getElements('.answer-options input'));
+    UI_Interface.replaceChildren(UI_Interface.getElements('.answerOptions')[0], elementsCreated);
+    const optionElementsArray = Array.from(UI_Interface.getElements('.answerOptions input'));
     UI_Interface.addEventListenerToElements(optionElementsArray, ['change'], [function () {
         quizzerDataOperation.checkAnswer(UI_Interface.getInputValue([this])[0], questionIndex)
     }]);
@@ -55,25 +57,25 @@ function renderQuizOnUI() {
         UI_Interface.setAttributes([optionElementsArray[selectedOptionIndex]], ['checked'], ['']);
     }
 
-    (!questionIndex) ? UI_Interface.addClassToElements([UI_Interface.getElements('#prev-btn')[0]], 'invisible') : UI_Interface.removeClassFromElements([UI_Interface.getElements('#prev-btn')[0]], 'invisible');
+    (!questionIndex) ? UI_Interface.addClassToElements([UI_Interface.getElements('#prev')[0]], 'invisible') : UI_Interface.removeClassFromElements([UI_Interface.getElements('#prev-btn')[0]], 'invisible');
 
-    (questionIndex === questionsData.length - 1) ? UI_Interface.addClassToElements([UI_Interface.getElements('#next-btn')[0]], 'invisible') : UI_Interface.removeClassFromElements([UI_Interface.getElements('#next-btn')[0]], 'invisible');
+    (questionIndex === questionsData.length - 1) ? UI_Interface.addClassToElements([UI_Interface.getElements('#next-btn')[0]], 'invisible') : UI_Interface.removeClassFromElements([UI_Interface.getElements('#next')[0]], 'invisible');
 }
 
-UI_Interface.addEventListenerToElements([UI_Interface.getElements('#prev-btn')[0]], ['click'], [function (event) {
+UI_Interface.addEventListenerToElements([UI_Interface.getElements('#prev')[0]], ['click'], [function (event) {
     questionIndex = (questionIndex > 0) ? --questionIndex : questionIndex;
     renderQuizOnUI();
     event.preventDefault();
 }
 ]
 );
-UI_Interface.addEventListenerToElements([UI_Interface.getElements('#next-btn')[0]], ['click'], [function (event) {
+UI_Interface.addEventListenerToElements([UI_Interface.getElements('#next')[0]], ['click'], [function (event) {
     questionIndex = (questionIndex < questionsData.length - 1) ? ++questionIndex : questionIndex;
     renderQuizOnUI();
     event.preventDefault();
 }]
 );
-UI_Interface.addEventListenerToElements([UI_Interface.getElements('#submit-btn')[0]], ['click'], [function (event) {
+UI_Interface.addEventListenerToElements([UI_Interface.getElements('#submitBtn')[0]], ['click'], [function (event) {
     quizzerDataOperation.calculateScoresAndEndQuiz();
     event.preventDefault();
 }])
