07e1f11721debf0ab837dfa55e4764c91e95fa91
Ridwan Abidoye <ridwan.abidoye@smartflowtech.com>
2021-11-30 13:23:46 +0100

    added results data schema for testing

---
+++ app/data/resultsData.js
@@ -0,0 +1,32 @@
+const resultData = [
+    {
+        timeStamp: '2011-10-05T14:48:00.000Z',
+        candidateEmail: 'abidoyeridwan@smartflowtech.com',
+        score: '8/10'
+    },
+    {
+        timeStamp: '2011-10-10T14:48:00.000Z',
+        candidateEmail: 'abidoyeridwan@smartflowtech.com',
+        score: '8/10'
+    },
+    {
+        timeStamp: '2011-11-07T14:48:00.000Z',
+        candidateEmail: 'abidoyeridwan@smartflowtech.com',
+        score: '8/10'
+    },
+    {
+        timeStamp: '2011-11-08T11:48:00.000Z',
+        candidateEmail: 'abidoyeridwan@smartflowtech.com',
+        score: '8/10'
+    },
+    {
+        timeStamp: '2012-02-08T12:48:00.000Z',
+        candidateEmail: 'abidoyeridwan@smartflowtech.com',
+        score: '8/10'
+    },
+    {
+        timeStamp: '2012-04-05T09:48:00.000Z',
+        candidateEmail: 'abidoyeridwan@smartflowtech.com',
+        score: '8/10'
+    }
+]
