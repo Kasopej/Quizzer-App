2eea12134153193e51060d880ac21568f0eded2e
Kasope Johnson <kasopej@gmail.com>
2021-12-09 11:20:27 +0100

    Added comments to quiz-setup.js

--- app/scripts/quiz-setup.js
+++ app/scripts/quiz-setup.js
@@ -19,12 +19,16 @@ const URL_Helper = new URL_HelperClass();
 const clipBoardObj = new ClipboardClass();
 
 // Import node objects from DOM
-const questionQtyElement = ui_Interface.getElements("#amountOfQuestions")[0];
-const selectCategoryElement = ui_Interface.getElements("#categorySelect")[0];
-const selectDifficultyElement =
+const questionQtyInputElement =
+  ui_Interface.getElements("#amountOfQuestions")[0];
+const categorySelectElement = ui_Interface.getElements("#categorySelect")[0];
+const difficultySelectElement =
   ui_Interface.getElements("#difficultySelect")[0];
-const selectTypeElement = ui_Interface.getElements("#typeSelect")[0];
-const selectTimingElement = ui_Interface.getElements("#timingSelect")[0];
+const typeSelectElement = ui_Interface.getElements("#typeSelect")[0];
+const timingSelectElement = ui_Interface.getElements("#timingSelect")[0];
+const difficultyOptionElements = Array.from(
+  ui_Interface.getElements("#difficultySelect option")
+);
 const typeOptionElements = Array.from(
   ui_Interface.getElements("#typeSelect option")
 );
@@ -32,9 +36,6 @@ const timingOptionElements = Array.from(
   ui_Interface.getElements("#timingSelect option")
 );
 const submitButtonElement = ui_Interface.getElements("#submitBtn")[0];
-const difficultyOptionElements = Array.from(
-  ui_Interface.getElements("#difficultySelect option")
-);
 const testExpirationDateElement = ui_Interface.getElements("#expiryDate")[0];
 
 //Make call for categories and attach them to category form element
@@ -48,12 +49,13 @@ quizzerData.getData("Quiz Categories").forEach((categoryObj) => {
   const optionElement = ui_Interface.createElements("option");
   ui_Interface.setAttributes([optionElement], ["value"], [categoryObj.id]);
   ui_Interface.attachText([optionElement], [categoryObj.name]);
-  ui_Interface.attachElements(selectCategoryElement, optionElement);
+  ui_Interface.attachElements(categorySelectElement, optionElement);
 });
 const categoryOptionElements = Array.from(
   ui_Interface.getElements("#categorySelect option")
 );
 
+//Check that categories data is saved/available in app data
 if (
   quizzerDataOperation.isDataAvailable(
     quizzerData,
@@ -61,18 +63,20 @@ if (
     "Quiz Categories"
   )
 ) {
-  //Checks if categories have been successfully saved in app. Returns boolean
   ui_Interface.removeElement(
     ui_Interface.getElements(".category-options-spinner")[0]
   );
 }
+
 function checkAndValidateQuantityInput() {
-  //Checks number of questions available based on current form selection. Prevents submission while checking
+  /*Checks number of questions available, based on current form selection. Prevents submission while checking 
+  Then validates quantity selected by user to ensure it is not more than available quantity
+  */
   ui_Interface.setAttributes([submitButtonElement], ["disabled"], [""]);
   quizzerDataOperation
     .qtyOfQuestionsAvailable(
-      categoryOptionElements[selectCategoryElement.selectedIndex].value,
-      difficultyOptionElements[selectDifficultyElement.selectedIndex].value
+      categoryOptionElements[categorySelectElement.selectedIndex].value,
+      difficultyOptionElements[difficultySelectElement.selectedIndex].value
     )
     .then((quantity) => {
       ui_Interface.attachText(
@@ -90,26 +94,27 @@ function checkAndValidateQuantityInput() {
 function validateQuantityInput() {
   //Prevent user from exceeding limits for available number of questions
   handlerHelpers.limitNumericalEntry.call(
-    questionQtyElement,
+    questionQtyInputElement,
     [quizzerData.getConfigData("numberOfQuestionsAvailableInSelection"), 1],
     ["max", "min"]
   );
 }
 
 function save_UI_Config_Entries(event) {
-  //On form submit, save form selections to configData
+  //On form submit action, save form selections to configData
   const selectedCategoryOptionElement =
-    categoryOptionElements[selectCategoryElement.selectedIndex];
+    categoryOptionElements[categorySelectElement.selectedIndex];
   const selectedDifficultyOptionElement =
-    difficultyOptionElements[selectDifficultyElement.selectedIndex];
+    difficultyOptionElements[difficultySelectElement.selectedIndex];
   const selectedTypeOptionElement =
-    typeOptionElements[selectTypeElement.selectedIndex];
+    typeOptionElements[typeSelectElement.selectedIndex];
   const selectedTimingOptionElement =
-    timingOptionElements[selectTimingElement.selectedIndex];
+    timingOptionElements[timingSelectElement.selectedIndex];
   let [candidatesEmails, numberOfQuestions] = ui_Interface.getInputValue([
     ui_Interface.getElements("#candidatesEmails")[0],
-    questionQtyElement,
+    questionQtyInputElement,
   ]);
+  //save actions
   quizzerData.updateConfigData(["amount", numberOfQuestions]);
   handlerHelpers.helpSaveData(
     quizzerData.updateConfigData,
@@ -141,24 +146,30 @@ function save_UI_Config_Entries(event) {
     "expiryDate",
     testExpirationDateElement.valueAsNumber
   );
+  //Emails entry requires processing before save
   processEmailEntries(candidatesEmails);
   event.preventDefault();
 }
 
 function processEmailEntries(candidatesEmails) {
-  //Validate emails and print links (if emails valid). Copy configuration data to each email link
+  //Validate emails and print links (if emails valid). Generate unique configuration link for each email
   const modalBodyElement = ui_Interface.getElements("#quiz-link-modal p")[0];
   ui_Interface.replaceHTML([modalBodyElement], [""]);
 
   candidatesEmails = candidatesEmails.trim();
   const candidatesEmailsArray = candidatesEmails.split(",");
+  //Find any invalid emails
   let invalidEmail = candidatesEmailsArray.find((email) => {
-    let pos = -1;
+    //loops over each email to check the number of at signs. There should be only one
+    let positionOfAtSign = -1;
     let numberOfAtSign = 0;
-    while ((pos = email.indexOf("@", pos + 1)) !== -1) {
+    while (
+      (positionOfAtSign = email.indexOf("@", positionOfAtSign + 1)) !== -1
+    ) {
       numberOfAtSign++;
     }
     return (
+      //If an email conforms to any of these conditions, it is invalid
       !email.includes(".") ||
       email.includes("@.") ||
       email.startsWith("@") ||
@@ -238,9 +249,9 @@ function processEmailEntries(candidatesEmails) {
 ui_Interface.addEventListenerToElements(
   [
     submitButtonElement,
-    questionQtyElement,
-    selectCategoryElement,
-    selectDifficultyElement,
+    questionQtyInputElement,
+    categorySelectElement,
+    difficultySelectElement,
   ],
   ["click", "input", "change"],
   [save_UI_Config_Entries, validateQuantityInput, checkAndValidateQuantityInput]
