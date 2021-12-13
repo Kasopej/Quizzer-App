7a14a9672c58a082d93c83623134b01ecd68900f
Ridwan Abidoye <ridwan.abidoye@smartflowtech.com>
2021-12-13 23:54:55 +0100

    login and sign up page added

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,3 +1,24 @@
 //import UserControl from "../Modules/util/user-control.js";
 
 function login(entry) {}
+function openModal(id) {
+  const modals = document.querySelectorAll(".cusModal");
+  for (let i = 0; i < modals.length; i++) {
+    if (modals[i].id == id) {
+      modals[i].classList.add("cusShow");
+    } else {
+      modals[i].classList.remove("cusShow");
+    }
+  }
+}
+
+function togglePass(el) {
+  const input = el.previousElementSibling;
+  if (input.type == "password") {
+    input.type = "text";
+    el.innerHTML = "Hide";
+  } else {
+    input.type = "password";
+    el.innerHTML = "Show";
+  }
+}
